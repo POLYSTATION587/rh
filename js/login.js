389730c96c3109de355a712a5419a2b3e4849aa8
CHARLES <polystation587@gmail.com>
2023-01-03 14:01:21 -0400

    implementação de mascara CPF

---
+++ js/login.js
@@ -0,0 +1,11 @@
+var maskCpfOuCnpj = IMask(document.getElementById('cpfcnpj'), {
+    mask:[
+        {
+            mask: '000.000.000-00',
+            maxLength: 11
+        },
+        {
+            mask: '00.000.000/0000-00'
+        }
+    ]
+});
